0464087b3499280754947e38834439a388e66e7c
zhoufan <zhoufan@tsaodai.com>
2025-08-14 21:05:07 +0800

    [问题/需求描述]:更新加载使用到的模块 [解决方法]:更新加载使用到的模块 [影响范围]:无 [需求编号]:无
    Change-Id: I4b914bb33d19b35beab98c05f619b6e3c3885a98

--- domain/components/python_api.cc
+++ domain/components/python_api.cc
@@ -4,6 +4,16 @@
 bool TradeDate::GetLoginDate(const std::string &timestring, char *login_date) {
   bool ret = true;
   pybind11::gil_scoped_acquire acquire;
+  pybind11::module_ importlib = pybind11::module_::import("importlib");
+  pybind11::module_ sys = pybind11::module_::import("sys");
+
+  auto modules = {"ticknature.trade_date"};
+  for (auto &module : modules) {
+    if (sys.attr("modules").contains(module)) {
+      importlib.attr("reload")(sys.attr("modules")[module]);
+    }
+  }
+
   pybind11::object tradedate = pybind11::module::import("ticknature.trade_date").attr("tradedate");
 
   pybind11::object obj = tradedate.attr("get_login_date")(timestring);
